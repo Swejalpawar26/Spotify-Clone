076ba9e800a10a184dec0259e03a9b64abe1a71e
Rinika Banerjee <banerjeetitan72@gmail.com>
2025-10-06 17:02:50 +0530

    Started writing javascript

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Writing javascript");
+
+async function main(){
+    let a=await fetch("http://127.0.0.1:5500/songs/");
+    let response=await a.text();
+    console.log(response);
+    let div=document.createElement("div")
+    div.innerHTML=response
+}
+
+main();
+
