b4fa5b470b2a0ad42f3ccde150897c4d3a8d6f0b
Rinika Banerjee <banerjeetitan72@gmail.com>
2025-10-08 19:07:49 +0530

    Fetched songs using API

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 console.log("Writing javascript");
 
-async function main(){
+async function getsongs(){
     let a=await fetch("http://127.0.0.1:5500/songs/");
     let response=await a.text();
     console.log(response);
     let div=document.createElement("div")
-    div.innerHTML=response
+    div.innerHTML=response;
+    let as=div.getElementsByTagName("a")
+    console.log(as);
+    let songs=[]
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs;
 }
+
+async function main() {
+    //get list of all songs
+    let songs=await getsongs();
+    console.log(songs);
+
+    //play first song
+    var audio=new Audio(songs[0])
+    audio.play()
+}  
 
 main();
 
