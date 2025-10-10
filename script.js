07c5c87ea9fe9116782ea66af49ffdecbade5e28
Rinika Banerjee <banerjeetitan72@gmail.com>
2025-10-10 21:01:53 +0530

    Created Sing list

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 console.log("Writing javascript");
 
-async function getsongs(){
-    let a=await fetch("http://127.0.0.1:5500/songs/");
-    let response=await a.text();
-    console.log(response);
-    let div=document.createElement("div")
-    div.innerHTML=response;
-    let as=div.getElementsByTagName("a")
-    console.log(as);
-    let songs=[]
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if(element.href.endsWith(".mp3")){
-            songs.push(element.href.split("/songs/")[1])
-        }
+async function getsongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div")
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a")
+  console.log(as);
+  let songs = []
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1])
     }
-    return songs;
+  }
+  return songs;
 }
 
 function cleanSongName(song) {
@@ -47,25 +47,38 @@ function cleanSongName(song) {
 
 
 async function main() {
-    //get list of all songs
-    let songs=await getsongs();
-    console.log(songs);
+  //get list of all songs
+  let songs = await getsongs();
+  console.log(songs);
 
-    songUL=document.querySelector(".songlist").getElementsByTagName("ul")[0]
-    for (const song of songs) {
-        songUL.innerHTML=songUL.innerHTML+`<li>${cleanSongName(song)}</li>`
-    }
+  songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    songUL.innerHTML = songUL.innerHTML +
+
+      `<li><img class="invert" src="music.svg" alt="">
+                            <div class="songinfo">
+                                <div> ${cleanSongName(song)}</div>
+                                <div>Artist Name</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div>
+                        </li>`
 
 
-    //play first song
-    var audio=new Audio(songs[0])
-    //audio.play()
+  }
 
-    audio.addEventListener("ontimeupdate",()=>{
-        let duration=audio.duration;
-        console.log(duration); 
-    })
-}  
+
+  //play first song
+  var audio = new Audio(songs[0])
+  //audio.play()
+
+  audio.addEventListener("ontimeupdate", () => {
+    let duration = audio.duration;
+    console.log(duration);
+  })
+}
 
 main();
 
